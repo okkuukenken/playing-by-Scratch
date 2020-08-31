5a8e65ade1f66691959bc5b859e97a90a92d1333
okkuukenken <math_programming_otaku@yahoo.co.jp>
2020-08-31 17:18:48 +0900

    commit

---
+++ Step 5/convert.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<fstream>
+int main(){
+    std::ifstream ifs("music.txt");
+    std::ofstream ofs_pitch("pitch.txt"),ofs_until_second("until_second.txt");
+    int bpm;
+    ifs>>bpm;
+    std::string pitch;
+    double length,sum=0;
+    std::map<std::string,int>pitch2num={{"ド",60},{"レ",62},{"ミ",64},{"ファ",65},{"ソ",67},{"ラ",69},{"休",0}};
+    while(ifs>>pitch>>length){
+        sum+=length;
+        ofs_pitch<<pitch2num[pitch]<<std::endl;
+        ofs_until_second<<sum<<std::endl;
+    }
+    return 0;
+}
